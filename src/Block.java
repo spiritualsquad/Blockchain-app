438e57a4aeedb836f5850b9ae832076e5acde58b
spiritualsquad <93115878+spiritualsquad@users.noreply.github.com>
2024-05-16 22:20:38 -0400

    basic blockchain implementation done

--- src/Block.java
+++ src/Block.java
@@ -5,6 +5,7 @@ public class Block {
     public String previousHash;
     private String data;
     private long timeStamp;
+    private int nonce;
     //block constructor
     public Block (String data,String previousHash){
         this.data = data;
@@ -13,7 +14,16 @@ public class Block {
         this.hash =calculateHash();
     }
     public String calculateHash(){
-        String calculatedHash = StringUtil.applySha256(previousHash +Long.toString(timeStamp)+data);
+        String calculatedHash = StringUtil.applySha256(previousHash +Long.toString(timeStamp) +Integer.toString(nonce)+data);
         return calculatedHash;
+    }
+    //lets do some mining
+    public void mineBlock(int level){
+        String target = new String(new char[level]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, level).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
