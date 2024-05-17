bac9a95ec792f666bf0c31f218054ce0b9589215
spiritualsquad <93115878+spiritualsquad@users.noreply.github.com>
2024-05-16 22:05:05 -0400

    created blockchain and also json format

--- src/StartChain.java
+++ src/StartChain.java
@@ -1,11 +1,17 @@
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
 public class StartChain {
+    public static ArrayList<Block> blockchain =new ArrayList<Block>();
     public static void main(String[] args) {
-        //tests
-        Block bitcoinBlock = new Block("I wish i had bitcoin","69");
-        System.out.println("First hash:"+ bitcoinBlock.hash);
-        Block spiritBlock = new Block("This is a spirit lol",bitcoinBlock.hash);
-        System.out.println("Second hash"+spiritBlock.hash);
-        Block whateverBlock = new Block("This is a block",spiritBlock.hash);
-        System.out.println("Third has"+whateverBlock.hash);
+        //storing blocks into array list
+        blockchain.add(new Block("I wish i had bitcoin","69"));
+        blockchain.add(new Block("This is a spirit lol",blockchain.getLast().hash));
+        blockchain.add(new Block("This is a block",blockchain.getLast().hash));
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+
     }
 }
